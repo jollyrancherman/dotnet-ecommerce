034665cf3d0aac2e213f90ed0ea771b3569aa22d
jollyrancherman <anthony.rsc@gmail.com>
2023-12-08 12:10:51 -0800

    Continue working on SPECIFICATION PATTERN

--- Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4655ef870c4f48ceef23e890d0cdf9e2a14d8fb4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e76369755f40ae163c26643e005376ca752b7cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
