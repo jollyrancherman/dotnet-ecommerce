8c0726b32b214d94d7f1096e7f3276abdc17181b
jollyrancherman <anthony.rsc@gmail.com>
2023-12-15 11:58:28 -0800

    end of section9

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,7 +1,4 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { Product } from './models/products';
-import { Pagination } from './models/pagination';
 
 @Component({
   selector: 'app-root',
@@ -10,23 +7,8 @@ import { Pagination } from './models/pagination';
 })
 export class AppComponent implements OnInit {
   title = 'client';
-  products: Product[] = [];
 
-  constructor(private http: HttpClient) {}
+  constructor() {}
 
-  ngOnInit(): void {
-    this.http
-      .get<Pagination<Product[]>>('https://localhost:5001/api/products')
-      .subscribe({
-        next: (response) => {
-          this.products = response.data;
-        },
-        error: (error) => {
-          console.log(error);
-        },
-        complete: () => {
-          console.log('complete');
-        },
-      });
-  }
+  ngOnInit(): void {}
 }
